846ef9b6d50552afdb01ae968d93d338730f3696
Aman Khan <amanayankhan78@gmail.com>
2024-05-28 18:07:56 +0530

    Hero Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,96 @@
-import Image from "next/image";
+import CurveBling from "@/components/svg/CurveBling";
+import PlayIcon from "@/components/svg/PlayIcon";
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="h-screen overflow-hidden">
+      <div className="relative w-full h-full lg:h-[90vh]">
+        <video className="absolute top-1/2 left-1/2 w-full h-full object-cover transform -translate-x-1/2 -translate-y-1/2" autoPlay muted loop>
+          <source src="/video.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <div className="absolute h-[600px] w-[60vw] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
+          <div className=" h-full w-full">
+            <div className="relative flex items-center justify-between">
+              {/* Left Part */}
+              <div >
+                <img src="https://nomadlist.com/assets/laurel.svg?7" className=" filter invert hue-rotate-90" width={200} height={400} />
+
+                <div className="absolute top-[10px] left-[38px] flex flex-col justify-center font-light  text-white text-[12px]">
+                  <span className="text-center"> #1 Nomad Community</span>
+                  <div className="flex  justify-center gap-1 my-1">
+                    <img src="https://nomadlist.com/assets/star.svg?3" className="filter invert hue-rotate-90" width={10} height={20} />
+                    <img src="https://nomadlist.com/assets/star.svg?3" className="filter invert hue-rotate-90" width={10} height={20} />
+                    <img src="https://nomadlist.com/assets/star.svg?3" className="filter invert hue-rotate-90" width={10} height={20} />
+                    <img src="https://nomadlist.com/assets/star.svg?3" className="filter invert hue-rotate-90" width={10} height={20} />
+                    <img src="https://nomadlist.com/assets/star.svg?3" className="filter invert hue-rotate-90" width={10} height={20} />
+                  </div>
+                  <span className="text-center"> Since 2014</span>
+                </div>
+
+                <h1 className="text-[56px] mt-[20px] text-white font-bold w-[730px] leading-none">Go nomad and work remotely around the world</h1>
+                <p className="text-white mt-[20px] text-[21px] mb-[14px] font-medium w-[700px]">
+                  Join the #1 global community of 33,679 remote workers living and traveling around the world since 2014
+                </p>
+                <div className="flex relative">
+                  <div className="relative " >
+                    <img alt="Nomad List member" width="40" src="https://nomadlist.com/assets/img/users/775858cb52bf06d463717dc37898575a.jpg?1666720009" height="40" className="rounded-full border-white border-2 h-[40px] w-[40px]" />
+                  </div>
+                  <div className="relative -left-1" >
+                    <img alt="Nomad List member" width="40" src="https://nomadlist.com/assets/img/users/b9355c2757c6b4741b0f4def87d8a588.jpg?1666719817" height="40" className="rounded-full border-white border-2 h-[40px] w-[40px]" />
+                  </div>
+
+                  <div className="relative -left-4" >
+                    <img alt="Nomad List member" width="40" src="https://nomadlist.com/assets/img/users/2758342da737041f0e8adffa925789f5.jpg?1666718721" height="40" className="rounded-full border-white border-2 h-[40px] w-[40px]" />
+                  </div>
+                  <div className="relative  -left-5 " >
+                    <img alt="Nomad List member" width="40" src="https://nomadlist.com/assets/img/users/92b13bde13f6ab1acc871d86a5cb9d06.jpg?1666717610" height="40" className="rounded-full border-white border-2 h-[40px] w-[40px]" />
+                  </div>
+                  <div className="relative -left-6" >
+                    <img alt="Nomad List member" width="40" src="https://nomadlist.com/assets/img/users/b9355c2757c6b4741b0f4def87d8a588.jpg?1666719817" height="40" className="rounded-full border-white border-2 h-[40px] w-[40px]" />
+                  </div>
+                </div>
+
+                <div>
+
+
+                  <div className="text-white text-[21px] pt-[14px] mt-[10px] font-medium">
+                    🍹 <span className="underline">Attend 390 meetups/year</span> in 100+ cities
+                  </div>
+                  <div className="text-white text-[21px] pt-[14px]  font-medium">
+                    ❤️ <span className="underline" >Meet new people</span> for dating or making friends
+                  </div>
+                  <div className="text-white text-[21px] pt-[14px]  font-medium">
+                    🧪 <span className="underline" >Research destinations</span> and find your best place to live
+                  </div>
+                  <div className="text-white text-[21px] pt-[14px]  font-medium">
+                    🌎 <span className="underline" >Keep track of your travels</span> and record where you've been
+                  </div>
+                  <div className="text-white text-[21px] pt-[14px]  font-medium">
+                    💬 <span className="underline" >Join Nomad List chat</span> and find your community on the road
+                  </div>
+                </div>
+              </div>
+
+
+              <div className="relative h-[320px] w-[350px] bg-white rounded-lg p-6">
+
+                <img src="https://videodelivery.net/ed83ec0012330711ea783fabd47efdff/thumbnails/thumbnail.jpg" className="rounded-lg" />
+
+                <PlayIcon />
+
+
+                <input placeholder="Type your email..." type="text" className="text-[16px] my-2 border-2 border-gray-200 w-full rounded-lg placeholder:text-black placeholder:font-light placeholder:text-[16px] p-2" />
+                <button className="bg-[#ff4742] text-white w-full p-2 rounded-lg">Go nomad -- </button>
+
+              </div>
+
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <CurveBling />
+    </div>
   );
 }
