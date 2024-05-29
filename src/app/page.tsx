202c935c37bd707bca9c4bc28ad9bc37cc3ebb00
Aman Khan <amanayankhan78@gmail.com>
2024-05-29 11:14:46 +0530

    responsive curve

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -81,7 +81,7 @@ export default function Home() {
                 <PlayIcon />
 
 
-                <input placeholder="Type your email..." type="text" className="text-[16px] my-2 border-2 border-gray-200 w-full rounded-lg placeholder:text-black placeholder:font-light placeholder:text-[16px] p-2" />
+                <input placeholder="Type your email..." type="text" className="text-[16px] my-2 border-2 border-gray-200 w-full rounded-lg placeholder:text-black placeholder:font-light placeholder:text-[16px] p-2 focus:ring-white" />
                 <button className="bg-[#ff4742] text-white w-full p-2 rounded-lg">Go nomad -- </button>
 
               </div>
