90debeb74f6a265082ae3415a977eca335615ef3
Aman Khan <amanayankhan78@gmail.com>
2024-06-03 14:14:31 +0530

    Create PicsDisplay.tsx

---
+++ src/components/SearchFilter/PicsDisplay.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const picsArray = [
+    'https://nomadlist.com/assets/img/places/playa-del-carmen-mexico.jpg',
+    'https://nomadlist.com/assets/img/places/ko-samui-thailand.jpg',
+    'https://nomadlist.com/assets/img/places/tokyo-japan.jpg',
+    'https://nomadlist.com/assets/img/places/bogota-colombia.jpg',
+    'https://nomadlist.com/assets/img/places/prague-czechia.jpg',
+    'https://nomadlist.com/assets/img/places/madrid-spain.jpg',
+    'https://nomadlist.com/assets/img/places/rio-de-janeiro-brazil.jpg',
+    'https://nomadlist.com/assets/img/places/munich-germany.jpg'
+]
+
+
+const PicsDisplay = () => {
+  return (
+    <div>PicsDisplay</div>
+  )
+}
+
+export default PicsDisplay
