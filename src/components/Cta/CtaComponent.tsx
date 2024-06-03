975884d183d6fa0ea6afb9cb2c31b4d5d9d0e81d
Aman Khan <amanayankhan78@gmail.com>
2024-06-03 11:54:28 +0530

    Final Hero Section

--- src/components/Cta/CtaComponent.tsx
+++ src/components/Cta/CtaComponent.tsx
@@ -3,9 +3,9 @@ import PlayIcon from "@/components/svg/PlayIcon";
 
 const CtaComponent = () => {
   return (
-    <div className="flex-1 mt-10 z-50">
+    <div className="flex-1 w-[100%] translate-y-[50%] md:-translate-y-[40%] md:max-w-[350px] mt-10 z-50">
 
-      <div className="relative  max-w-[350px] mx-auto bg-white rounded-lg p-6">
+      <div className="relative  md:mx-auto bg-white rounded-lg p-6">
         <img
           src="https://videodelivery.net/ed83ec0012330711ea783fabd47efdff/thumbnails/thumbnail.jpg"
           className="rounded-lg hidden md:block"
@@ -16,7 +16,7 @@ const CtaComponent = () => {
         <input
           placeholder="Type your email..."
           type="text"
-          className="text-[16px] my-2 border-2 border-gray-200 w-full rounded-lg placeholder:text-black placeholder:font-light placeholder:text-[16px] p-2 focus:ring-white"
+          className="text-[16px] my-2 border-2 border-gray-200 w-full rounded-lg placeholder:text-black placeholder:font-light placeholder:text-[14px] p-2 focus:ring-white"
         />
         <button className="bg-[#ff4742] text-white w-full p-2 rounded-lg">
           Go nomad --{" "}
--- src/components/Info/LinksComponent.tsx
+++ src/components/Info/LinksComponent.tsx
@@ -3,19 +3,19 @@ import React from 'react'
 const LinksComponent = () => {
   return (
     <div className='flex flex-col'>
-      <p className="text-white text-[21px] pt-[14px] mt-[10px] font-normal">
+      <p className="text-white text-[16px] md:text-[21px] pt-[14px] mt-[10px] font-normal">
         🍹<span className="underline text-nowrap">Attend 390 meetups/year</span>&nbsp; in 100+ cities.
       </p>
-      <p className="text-white text-[21px] pt-[14px]  font-normal">
+      <p className="text-white text-[16px] md:text-[21px] pt-[14px]  font-normal">
         ❤️<span className="underline text-nowrap" >Meet new people</span>&nbsp; for dating or making friends.
       </p>
-      <p className="text-white hidden md:block text-[21px] pt-[14px]  font-normal">
+      <p className="text-white hidden md:block text-[16px] md:text-[21px] pt-[14px]  font-normal">
         🧪<span className="underline text-nowrap" >Research destinations</span>&nbsp; and find your best place to live.
       </p>
-      <p className="text-white hidden md:block text-[21px] pt-[14px]  font-normal">
+      <p className="text-white hidden md:block text-[16px] md:text-[21px] pt-[14px]  font-normal">
         🌎<span className="underline text-nowrap" >Keep track of your travels</span>&nbsp;  and record where you've been.
       </p>
-      <p className="text-white hidden md:block text-[21px] pt-[14px] justify-start  font-normal">
+      <p className="text-white hidden md:block text-[16px] md:text-[21px] pt-[14px] justify-start  font-normal">
         💬<span className="underline text-nowrap" >Join Nomad List chat</span>&nbsp; and find your community on the road.
       </p>
     </div>
